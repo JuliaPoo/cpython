3d85d2df52b63aa5cd9816d932fb127ddc2f1aa6
Lisa Roach <lisroach@fb.com>
2019-03-06 19:12:54 -0800

    Removes blank line.

--- Lib/test/test_json/test_dump.py
+++ Lib/test/test_json/test_dump.py
@@ -3,7 +3,6 @@ from test.test_json import PyTest, CTest
 
 from test.support import bigmemtest, _1G
 
-
 class TestDump:
     def test_dump(self):
         sio = StringIO()
@@ -57,6 +56,7 @@ class TestDump:
         d = D()
         d[1337] = "true.dat"
         self.assertEqual(self.dumps(d, sort_keys=True), '{"1337": "true.dat"}')
+
 
 class TestPyDump(TestDump, PyTest): pass
 
